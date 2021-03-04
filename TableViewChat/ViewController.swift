6e2ade2a0f1c1b1a06ec341ab85c8e2294041c52
IvanCo <ivanack2015@gmail.com>
2021-03-04 20:58:00 +0200

    Initial Commit

---
+++ TableViewChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewChat
+//
+//  Created by Ivan Ivanov on 3/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
